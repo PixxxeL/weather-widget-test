2351e85378990be30d80f8a417b5c1cdd591c4e4
PixxxeL <piksel@mail.ru>
2017-07-15 11:33:26 +0500

    Finish CRUD for widgets

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,24 @@
-console.log($.fn.jquery);
+var delete_item = function () {
+    $('a.delete-item').on('click', function (e) {
+        e.preventDefault();
+        if (confirm('Вы действительно хотите безвозвратно удалить объект?')) {
+            location.href = $(this).attr('href');
+        }
+    });
+};
+
+var get_widget_code = function () {
+    $('.get-widget-code').on('click', function (e) {
+        e.preventDefault();
+        alert(
+            'Скопируйте код и вставьте его\n' +
+            'в любом месте страницы внутри тега `body`:\n\n' +
+            '<script src="' + $(this).attr('href') + '"></script>'
+        );
+    });
+};
+
+$( function () {
+    delete_item();
+    get_widget_code();
+});
--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 import { app, passport, errStr } from  './init';
 import { Widget, CITY_MAPPING, DAYS_CHOISES } from './models/Widget';
+import config from '../config.json';
 
 
 app.get('/', (req, res) => {
@@ -31,22 +32,27 @@ app.get('/widgets', (req, res) => {
     Widget.find({user:req.user}, (err, widgets) => {
         res.render('widgets/list.pug', {
             user : req.user,
+            baseUrl : config.baseUrl,
+            cities : CITY_MAPPING,
             widgets : widgets || []
         });
     });
 });
 
+const getWidgetFromData = (user, widget, mode) => {return {
+    user : user,
+    widget : widget,
+    cities : CITY_MAPPING,
+    days : DAYS_CHOISES,
+    mode : mode
+};};
+
 app.get('/widgets/add', (req, res) => {
     if (!req.user) {
         return res.redirect('/');
     }
-    console.log('Request widget add page...');
-    res.render('widgets/add.pug', {
-        user : req.user,
-        cities : CITY_MAPPING,
-        days : DAYS_CHOISES,
-        mode : 'Добавить'
-    });
+    console.log('Request widget add form page...');
+    res.render('widgets/form.pug', getWidgetFromData(req.user, null, 'Добавить'));
 });
 
 
@@ -54,19 +60,56 @@ app.post('/widgets/add', (req, res) => {
     if (!req.user) {
         return res.redirect('/');
     }
-    console.log('Request widget create page...');
+    console.log('Request widget add page...');
     let widget = req.body;
     widget.user = req.user;
     Widget.create(widget, function (err, data) {
         if (!err) {
             return res.redirect('/widgets');
+        } else {
+            res.render('widgets/form.pug', getWidgetFromData(req.user, data, 'Добавить'));
         }
     });
-    res.render('widgets/add.pug', {
-        user : req.user,
-        widget : widget,
-        cities : CITY_MAPPING,
-        days : DAYS_CHOISES,
-        mode : 'Добавить'
+});
+
+app.get('/widgets/:id/edit', (req, res) => {
+    if (!req.user) {
+        return res.redirect('/');
+    }
+    console.log('Request widget edit form page...');
+    Widget.findById(req.params.id, function (err, data) {
+        if (!err) {
+            res.render('widgets/form.pug', getWidgetFromData(req.user, data, 'Редактировать'));
+        } else {
+            return res.redirect('/widgets');
+        }
     });
+});
+
+app.post('/widgets/:id/edit', (req, res) => {
+    if (!req.user) {
+        return res.redirect('/');
+    }
+    console.log('Request widget edit page...');
+    Widget.update({ _id : req.params.id }, { $set : req.body }, function (err, data) {
+        if (!err) {
+            return res.redirect(`/widgets`);
+        } else {
+            return res.redirect(`/widgets/${data.id}/edit`);
+        }
+    });
+});
+
+app.get('/widgets/:id/del', (req, res) => {
+    if (!req.user) {
+        return res.redirect('/');
+    }
+    console.log('Request widget del page...');
+    Widget.findById(req.params.id).remove((err, data) => {
+        res.redirect('/widgets');
+    });
+});
+
+app.get('/widgets/:id.js', (req, res) => {
+    res.send('File content will be here...'); // TODO
 });
