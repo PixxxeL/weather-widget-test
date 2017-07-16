e18d32990d2c52e1fa336a89a838975145c9aa02
PixxxeL <piksel@mail.ru>
2017-07-16 16:06:55 +0500

    Rewrite get weather on promises

--- server/utils/get-weather.js
+++ server/utils/get-weather.js
@@ -1,4 +1,4 @@
-import request from 'request';
+import request from 'request-promise-native';
 import Redis from 'ioredis';
 import config from '../../config.json';
 import { CITY_MAPPING } from '../models/Widget';
@@ -7,15 +7,14 @@ const URL = 'http://api.openweathermap.org/data/2.5/weather?APPID=';
 
 const redis = new Redis();
 
-for (let [ru, en] of Object.entries(CITY_MAPPING)) {
+Promise.all(Object.keys(CITY_MAPPING).map((ru) => {
+    const en = CITY_MAPPING[ru];
     let url = `${URL}${config.openWeatherId}&q=${en}&lang=ru&units=metric`;
-    request(url, (error, response, body) => {
+    return request(url, (error, response, body) => {
         console.log(`Recieve ${ru}...`);
         if (!error && response.statusCode == 200) {
             redis.set(en, body);
             console.log(`  and save...`);
         }
     });
-}
-
-setTimeout(process.exit, config.getWeatherTimeout);
+})).then(process.exit);
