55d134df98d6fc128d3c0ba2001b911e9e1e53d7
Ivan Sergeev <ivan.n.sergeev@gmail.com>
2020-03-29 13:56:28 +0500

    Fix data

--- server/models/Widget.js
+++ server/models/Widget.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const CITY_MAPPING = {
-    'Москва' : 'Moskow',
+    'Москва' : 'Moscow',
     'Санкт-Петербург' : 'Saint Petersburg',
     'Нижний Новгород' : 'Nizhniy Novgorod'
 };
